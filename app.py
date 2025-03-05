e147214522df064f96ef5f43433e54938ebb58ab
Galoev <galoyev.ilkin@gmail.com>
2025-03-05 17:33:47 +0300

    add web-service

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ app = FastAPI()
 
 tasks = ['1. Task', '2. Task', '3. Hello world']
 
+
 @app.get("/")
 def read_tasks():
     return {"tasks": tasks}
+
 
 @app.post("/add")
 def add_task(task: str):
